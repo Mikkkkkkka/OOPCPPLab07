d9ecc6fca1adf3eac19714ff3b3c155fe71d84c9
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2024-04-18 23:39:50 +0300

    Почему так нхй

--- main.cpp
+++ main.cpp
@@ -4,35 +4,31 @@
 #include <algorithm>
 #include <string>
 
+void out(int x)
+{
+    std::cout << x << ' ';
+}
 
 int main()
 {
     RingBuffer<int, 8> rb;
 
+
     for (int i = 0; i < 8; ++i)
         rb.push_back(i);
     rb.print();
-    for (int i = 0; i < 10; i++)
-        std::cout << *std::find(rb.begin(), rb.end(), i) << ' ';
-    std::cout << '\n';
+    std::for_each(rb.begin(), rb.end(), out);
+    std::cout << '\n' << *std::find(rb.begin(), rb.end(), 8) << '\n';
 
     rb.push_back(8);
     rb.print();
-    for (int i = 0; i < 10; i++)
-        std::cout << *std::find(rb.begin(), rb.end(), i) << ' ';
-    std::cout << '\n';
+    std::for_each(rb.begin(), rb.end(), out);
+    std::cout << '\n' << *std::find(rb.begin(), rb.end(), 8) << '\n';
 
     rb.push_back(9);
     rb.print();
-    for (int i = 0; i < 10; i++)
-        std::cout << *std::find(rb.begin(), rb.end(), i) << ' ';
-    std::cout << '\n';
-
-    rb.push_back(9);
-    rb.print();
-    for (int i = 0; i < 10; i++)
-        std::cout << *std::find(rb.begin(), rb.end(), i) << ' ';
-    std::cout << '\n';
+    std::for_each(rb.begin(), rb.end(), out);
+    std::cout << '\n' << *std::find(rb.begin(), rb.end(), 9) << '\n';
 
     return 0;
 }
